7719f5ce02c0778be9ebee52a6ace5509b0bcde8
Suman Das <sumandasddm@gmail.com>
2022-08-02 22:51:09 +0530

    edit playlist added

--- controllers/adminPlayList.js
+++ controllers/adminPlayList.js
@@ -1,66 +1,78 @@
-const AdminPlayList = require("../models/adminPlayList")
+const AdminPlayList = require("../models/adminPlayList");
 exports.createAdminPlayList = async (req, res) => {
-    const { playListName, songs } = req.body;
-    const newPlayList = new AdminPlayList({
-        playListName: playListName,
-        songs: songs
-    })
-    try {
-        const playListData = await newPlayList.save();
+  const { playListName, songs } = req.body;
+  const newPlayList = new AdminPlayList({
+    playListName: playListName,
+    songs: songs,
+  });
+  try {
+    const playListData = await newPlayList.save();
 
-        return res.status(200).json({
-            error: false,
-            playListData
-        })
-    }
-    catch (err) {
-        return res.status(400).json({
-            error: true,
-            message: err
-        })
-    }
-}
+    return res.status(200).json({
+      error: false,
+      playListData,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: true,
+      message: err,
+    });
+  }
+};
 
 exports.getAllAdminPlayList = async (req, res) => {
-    try {
-
-        const allLists = await AdminPlayList.find();
-        return res.status(200).json({
-            error: false,
-            allLists
-        })
-
-    }
-    catch (err) {
-        return res.status(400).json({
-            error: true,
-            message: err
-        })
-    }
-}
+  try {
+    const allLists = await AdminPlayList.find();
+    return res.status(200).json({
+      error: false,
+      allLists,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: true,
+      message: err,
+    });
+  }
+};
 
 exports.getSongsInAdminAPlayList = (req, res) => {
-    AdminPlayList.findById(req.playListId).populate('songs').exec((err, playList) => {
-        if (err) {
-            return res.status(400).json({
-                error: true,
-                message: "Something went wrong !"
-            })
-        }
-        return res.status(200).json({
-            error: false,
-            playList
-        })
-    })
-}
+  AdminPlayList.findById(req.playListId)
+    .populate("songs")
+    .exec((err, playList) => {
+      if (err) {
+        return res.status(400).json({
+          error: true,
+          message: "Something went wrong !",
+        });
+      }
+      return res.status(200).json({
+        error: false,
+        playList,
+      });
+    });
+};
 
 // params functions
 exports.getSongsByPlayListIdParam = (req, res, next, id) => {
-    req.playListId = id;
-    next()
-}
+  req.playListId = id;
+  next();
+};
 
-
-
-
-
+exports.updateAdminPlayList = (req, res) => {
+  console.log(req.params);
+  AdminPlayList.findById(req.params.adminPlayListId, (err, playList) => {
+    if (err) {
+      return res.status(400).json({ error: true });
+    }
+    if (playList == null) return res.status(404).json({ error: true });
+    playList.playListName = req.body.playListName;
+    playList.songs = req.body.songs;
+    playList.save((err, newPlayList) => {
+      if (err) {
+        return res.status(400).json({ error: true, message: err.message });
+      }
+      return res.status(200).json({ error: false, newPlayList });
+    });
+  });
+  //   return res.status(200).json({ message: "hello" });
+};
--- routes/adminPlayList.js
+++ routes/adminPlayList.js
@@ -1,14 +1,25 @@
-const express = require("express")
-const { createAdminPlayList, getAllAdminPlayList, getSongsByPlayListIdParam, getSongsInAdminAPlayList } = require("../controllers/adminPlayList")
-const { isSignedIn } = require("../controllers/auth")
-const { getUserByIdParam } = require("../controllers/auth")
-const router = express.Router()
+const express = require("express");
+const {
+  createAdminPlayList,
+  getAllAdminPlayList,
+  getSongsByPlayListIdParam,
+  getSongsInAdminAPlayList,
+  updateAdminPlayList,
+} = require("../controllers/adminPlayList");
+const { isSignedIn } = require("../controllers/auth");
+const { getUserByIdParam } = require("../controllers/auth");
+const router = express.Router();
 
-router.param("userId", getUserByIdParam)
-router.param("adminPlayListId", getSongsByPlayListIdParam)
+router.param("userId", getUserByIdParam);
+router.param("adminPlayListId", getSongsByPlayListIdParam);
 
-router.post("/create/:userId", isSignedIn, createAdminPlayList)
-router.get("/all/:userId", isSignedIn, getAllAdminPlayList)
-router.get("/songs/:userId/:adminPlayListId", isSignedIn, getSongsInAdminAPlayList)
+router.post("/create/:userId", isSignedIn, createAdminPlayList);
+router.get("/all/:userId", isSignedIn, getAllAdminPlayList);
+router.get(
+  "/songs/:userId/:adminPlayListId",
+  isSignedIn,
+  getSongsInAdminAPlayList
+);
+router.post("/update/:userId/:adminPlayListId", updateAdminPlayList);
 
-module.exports = router
+module.exports = router;
